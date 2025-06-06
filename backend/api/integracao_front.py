3e7cc2978f03a7bf28284488aa335a69046e6648
Hugo Rios Brito <hugoba532@gmail.com>
2025-06-06 20:45:10 -0300

    versao simplificada e com modificacao na lógica de negócio

--- backend/api/integracao_front.py
+++ backend/api/integracao_front.py
@@ -1,164 +1,49 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-import hashlib
-from typing import List, Optional
-import random
-import uuid
 from backend.validacao.validacao import Validacao
 from backend.api.requisicao import Endpoints, Requisicao
-from backend.api.autenticacao import AutenticacaoJWT
 
-#API DE INTEGRAÇÃO COM O FRONTEND
+#INTEGRAÇÃO COM O FRONTEND
 
-app = FastAPI()
 validacao = Validacao()
-autenticacao = AutenticacaoJWT()
 
 end = Endpoints(url_base="http://localhost:9000")
 req = Requisicao(end)
 
-class Endereco(BaseModel):
-    rua: str
-    numero: str
-    complemento: Optional[str] = None
-    bairro: str
-    cidade: str
-    estado: str
-    cep: str
-
-class CadastroUsuario(BaseModel):
-    id_usuario: Optional[str] = None
-    nome: str
-    email: str
-    senha: str
-    telefone: Optional[str] = None
-    endereco: Endereco
-
-class LoginUsuario(BaseModel):
-    email: str
-    senha: str
-
-class Livro(BaseModel):
-    idLivro: int
-    titulo: str
-    autor: str
-
-class Pedido(BaseModel):
-    id_pedido: str
-    id_usuario: str
-
-class CalculoFrete(BaseModel):
-    cep: str
-
-
-@app.post("/v1/usuario/cadastrar/")
-async def cadastrar_usuario(usuario: CadastroUsuario):
+def cadastrar_usuario(usuario: dict[str, str]):
     try:
-        v_email = validacao.validar_email(usuario.email)
+        validacao.validar_email(usuario['email'])
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{e}")
+        raise Exception({e})
 
     try:
-        v_senha = validacao.validar_senha(usuario.senha)
+        validacao.validar_senha(usuario['senha'])
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{e}")
+        raise Exception(e)
 
-
-    salt = "salt_de_teste"
-    senha_usuario = usuario.senha+salt
-    usuario.senha = str(hashlib.md5(senha_usuario.encode()).hexdigest())
-
-    print("senha com hash: ", usuario.senha)
-
-    usuario.id_usuario = str(uuid.uuid4())
-    cadastro_banco = req.cadastrar(usuario.model_dump())
+    cadastro_banco = req.cadastrar(usuario)
 
     return cadastro_banco
 
-@app.post("/v1/usuario/logar/")
-async def login_usuario(usuario: LoginUsuario):
+def login_usuario(usuario: dict[str, str]):
 
-    email_usuario = usuario.email
-    senha_usuario = usuario.senha
-
-    salt = "salt_de_teste"
-    senha_usuario = senha_usuario+salt
-    usuario.senha = str(hashlib.md5(senha_usuario.encode()).hexdigest())
-    senha_usuario = usuario.senha
-
-    print("senha com hash: ", usuario.senha)
+    email_usuario = usuario['email']
+    senha_usuario = usuario['senha']
 
     if not email_usuario or not senha_usuario:
-        raise HTTPException(status_code=400, detail="Email e senha são obrigatórios")
+        raise Exception("Email e senha são obrigatórios")
 
-    login_banco = req.logar(usuario.model_dump())
-
-    token = autenticacao.criar_token(user_data=login_banco["usuario"])
-
-    login_banco["token"] = token
-    login_banco["tipo_token"] = "Bearer"
+    login_banco = req.logar(usuario)
 
     return login_banco
     
-
-@app.get("/v1/livros/")
-async def listar_livros():
+def listar_livros():
     livros = req.listar_biblioteca()
     if not livros:
-        raise HTTPException(status_code=404, detail="Nenhum livro encontrado")
+        raise Exception("Nenhum livro encontrado")
     return livros
 
-# Endpoints de carrinho
-@app.post("/v1/loja/carrinho/adicionar/{idLivro}")
-async def adicionar_ao_carrinho(idLivro: int, user: dict = Depends(autenticacao.verificar_token)):
-    if not idLivro:
-        raise HTTPException(status_code=400, detail="ID do livro é obrigatório")
-    
-    banco_adicionar_livro_carrinho = req.adicionar_no_carrinho(idLivro)
-    return banco_adicionar_livro_carrinho
+def realizar_download_livro(idLivro):
+    url_livro = req.realizar_download(idLivro)
+    if not url_livro:
+        raise Exception("Download do livro não encontrado")
+    return url_livro
 
-@app.delete("/v1/loja/carrinho/remover/{idLivro}")
-async def remover_do_carrinho(idLivro: int,  user: dict = Depends(autenticacao.verificar_token)):
-    if not idLivro:
-        raise HTTPException(status_code=400, detail="ID do livro é obrigatório")
-    
-    banco_remover_livro_carrinho = req.remover_do_carrinho(idLivro)
-    return banco_remover_livro_carrinho
-
-# Endpoints de pedido
-# quando o usuario cria o pedido, é sintetizado todo o carrinho em um só pedido com um id, e assim o usuário pode confirmar ou cancelar
-@app.get("/v1/loja/pedido/criar/")
-async def criar_pedido_(user: dict = Depends(autenticacao.verificar_token)):
-    idPedido = str(uuid.uuid1())
-    banco_criar_pedido = req.criar_pedido(idPedido)
-    return banco_criar_pedido
-
-@app.post("/v1/loja/pedido/confirmar/{idPedido}")
-async def confirmar_pedido(idPedido: str,  user: dict = Depends(autenticacao.verificar_token)):
-    if not idPedido:
-        raise HTTPException(status_code=400, detail="ID do pedido é obrigatório")
-    
-    banco_confirmar_pedido = req.confirmar_pedido(idPedido)
-    return banco_confirmar_pedido
-
-@app.delete("/v1/loja/pedido/cancelar/{idPedido}")
-async def cancelar_pedido(idPedido: str,  user: dict = Depends(autenticacao.verificar_token)):
-    if not idPedido:
-        raise HTTPException(status_code=400, detail="ID do pedido é obrigatório")
-    
-    banco_cancelar_pedido = req.cancelar_pedido(idPedido)
-    return banco_cancelar_pedido
-
-    
-"""
-# Endpoint de cálculo de prazo
-@app.post("/v1/calculo/prazo-entrega/")
-async def calcular_prazo_entrega(calculo: CalculoFrete):
-    prazo_entrega = int(random.randint(1,10))
-    frete = format(float(random.uniform(10.0, 50.0)), '.2f')
-    print(prazo_entrega)
-    return {
-        "prazo_entrega": f"{prazo_entrega} dias úteis",
-        "frete": frete
-    }
-"""
--- backend/api/requisicao.py
+++ backend/api/requisicao.py
@@ -1,10 +1,9 @@
 import requests
-from datetime import datetime
 #REQUISIÇÕES PARA API DO BACKEND JAVA QUE VAI RETORNAR OS DADOS DO BANCO DE DADOS
 
 mock_usuarios = {}
 mock_livros = [
-{
+            {
                 "idLivro": 1,
                 "nomeLivro": "Red: A História de Chapeuzinho Vermelho",
                 "autor": "Liesl Shurtliff",
@@ -68,7 +67,6 @@ mock_livros = [
 mock_carrinho = {}
 mock_pedidos = {}
 
-
 class Endpoints:
     def __init__(self, url_base):
         self.url_base = url_base
@@ -82,17 +80,8 @@ class Endpoints:
     def get_biblioteca_listar_endpoint(self):
         return f"{self.url_base}/v1/biblioteca/listar/"
 
-    def get_carrinho_adicionar_endpoint(self, idLivro):
-        return f"{self.url_base}/v1/carrinho/adicionar/{idLivro}"
-
-    def get_carrinho_remover_endpoint(self, idLivro):
-        return f"{self.url_base}/v1/carrinho/remover/{idLivro}"
-
-    def get_pedido_confirmar_endpoint(self, idPedido):
-        return f"{self.url_base}/v1/pedido/confirmar/{idPedido}"
-
-    def get_pedido_cancelar_endpoint(self, idPedido):
-        return f"{self.url_base}/v1/pedido/cancelar/{idPedido}"
+    def get_download_livro_endpoint(self, idLivro):
+        return f"{self.url_base}/v1/livro/download/{idLivro}"
 
 class Requisicao:
 
@@ -104,185 +93,34 @@ class Requisicao:
             return requests.get(url)
         elif metodo == "POST":
             return requests.post(url, data)
-        elif metodo == "DELETE":
-            return requests.delete(url)
-        raise ValueError("Método de requisição inválido! Use 'GET', 'POST' ou 'DELETE'.")
+        raise ValueError("Método de requisição inválido! Use 'GET', 'POST'")
 
     def listar_biblioteca(self):
         endpoint_url = self.endpoints.get_biblioteca_listar_endpoint()
 
-        return mock_livros
-        """      
         return self.enviar_requisicao(
             "GET",
             endpoint_url
-        )"""
+        )
 
     def cadastrar(self, data):
         endpoint_url = self.endpoints.get_cadastro_endpoint()
 
-        print(data)
-        print(data.get("email"))
-
-        for usuario in mock_usuarios.values():
-            if usuario.get("email") == data.get("email"):
-                return {"status":"erro","mensagem": "Usuário já cadastrado"}
-        
-        mock_usuarios[data.get("id_usuario")] = data
-
-        return {"status":"sucesso","mensagem": "Usuário cadastrado com sucesso", "usuario": data}
-        
-        """
         return self.enviar_requisicao(
             "POST",
             endpoint_url,
-            json=data
+            data=data
         )
-        """
+
 
     def logar(self, data):
         endpoint_url = self.endpoints.get_logar_endpoint()
-        
-        email_login = data.get("email")
-        senha_login = data.get("senha")
-        
-        for usuario in mock_usuarios.values():
-            if usuario.get("email") == email_login:
-                if usuario.get("senha") == senha_login:
-                    return {"status":"sucesso","mensagem": "Usuário logado com sucesso", "usuario": usuario}
-                else:
-                    return {"status":"erro","mensagem": "Email ou senha inválidos"}
-        
-        return {"status":"erro","mensagem": "Email ou senha inválidos"}
 
-        
-        """
         return self.enviar_requisicao(
             "POST",
             endpoint_url,
-            json=data
+            data=data
         )
-        """
 
-    def adicionar_no_carrinho(self, idLivro):
-        endpoint_url = self.endpoints.get_carrinho_adicionar_endpoint(idLivro=idLivro)
-        
-        for livro in mock_livros:
-            if livro["idLivro"] == idLivro:
-                mock_carrinho[idLivro] = livro
-                return {"status":"sucesso", "message": f"Livro {idLivro} adicionado ao carrinho", "livro": livro}
-
-        return {"status":"erro","mensagem": "Livro não encontrado"}
-        
-        """
-        return self.enviar_requisicao(
-            "POST",
-            endpoint_url,
-            json=data
-        )
-        """
-
-    def remover_do_carrinho(self, idLivro):
-        endpoint_url = self.endpoints.get_carrinho_remover_endpoint(idLivro=idLivro)
-        
-        for livro in mock_livros:
-            if idLivro in mock_carrinho:
-                livro = mock_carrinho.pop(idLivro)
-                return {"message": f"Livro {idLivro} removido do carrinho", "livro": livro}
-        return {"status":"erro","mensagem": "Livro não encontrado"}
-
-        """
-        return self.enviar_requisicao(
-            "DELETE",
-            endpoint_url,
-        )
-        """
-
-    def criar_pedido(self, idPedido):
-        endpoint_url = self.endpoints.get_pedido_confirmar_endpoint(idPedido=idPedido)
-        
-        if not mock_carrinho:
-            return {"status": "erro", "mensagem": "Carrinho vazio"}
-
-        itens_pedido = []
-        preco_total = 0.0
-        
-        for idLivro, livro in mock_carrinho.items():
-            preco = float(livro["preco"]) if isinstance(livro["preco"], str) else livro["preco"]
-
-            item = {
-                "idLivro": str(livro["idLivro"]),
-                "nomeLivro": livro["nomeLivro"],
-                "quantidade": 1, 
-                "preco": f"{preco:.2f}",
-                "subtotal": f"{preco:.2f}"
-            }
-            itens_pedido.append(item)
-            preco_total += preco
-
-        taxa_entrega = 5.40
-        preco_total += taxa_entrega
-
-        pedido = {
-            "status": "pendente",
-            "data": {
-                "idPedido": idPedido,
-                "tipoPedido": "Cartão",  # Cartão, Pix, Boleto
-                "precoTotal": f"{preco_total:.2f}",
-            },
-            "mensagem": "Pedido criado com sucesso"
-        }
-
-        mock_pedidos[idPedido] = pedido
-
-        return pedido
-
-    def confirmar_pedido(self, idPedido):
-        endpoint_url = self.endpoints.get_pedido_confirmar_endpoint(idPedido=idPedido)
-        
-        if not mock_carrinho:
-            return {"status": "erro", "mensagem": "Carrinho vazio"}
-
-        if idPedido not in mock_pedidos:
-            return {"status": "erro", "mensagem": "Pedido não encontrado"}
-
-        pedido = mock_pedidos[idPedido]
-
-        pedido["status"] = "confirmado"
-        pedido["mensagem"] = "Pedido confirmado com sucesso"
-
-        mock_carrinho.clear()
-
-        return pedido
-        
-        """
-        return self.enviar_requisicao(
-            "POST",
-            endpoint_url,
-            json=data
-        )
-        """
-
-    def cancelar_pedido(self, idPedido):
-        endpoint_url = self.endpoints.get_pedido_cancelar_endpoint(idPedido=idPedido)
-        
-        if idPedido not in mock_pedidos:
-            return {"status": "erro", "mensagem": "Pedido não encontrado"}
-
-        pedido = mock_pedidos[idPedido]
-
-        pedido["status"] = "cancelado"
-        pedido["mensagem"] = "Pedido cancelado com sucesso"
-
-        mock_pedidos.pop(idPedido)
-        mock_carrinho.clear()
-
-        return pedido
-        
-        
-        """
-        return self.enviar_requisicao(
-            "DELETE",
-            endpoint_url,
-        )
-        """
+    def realizar_download(self, idLivro):
+        endpoint_url = self.endpoints.get_download_livro_endpoint(idLivro)
